493f08ee667ecf7ce91be9dafa2e0fc15c51c834
Drabkjar1 <jaroslava.drabkova@gmail.com>
2022-04-30 11:20:25 +0200

    hamburger menu

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import './style.css';
 
-console.log('funguju!');
+const navBtnElm = document.querySelector('#nav-btn');
+const navElm = document.querySelector('nav');
+const navLinksElm = document.querySelectorAll('nav a');
+
+navBtnElm.addEventListener('click', () => {
+  navElm.classList.toggle('nav-closed');
+});
+
+navLinksElm.forEach((elm) => {
+  elm.addEventListener('click', () => {
+    navElm.classList.add('nav-closed');
+  });
+});
